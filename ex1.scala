958b8e4c62efc6d7219c23d59e7e2be86d1d4420
vrbloke <stanislawwis@gmail.com>
2020-10-26 17:38:50 +0100

    Done

--- ex1.scala
+++ ex1.scala
@@ -1,3 +1,36 @@
+import scala.math.{sin, cos, tan, sqrt, log, exp, pow}
+
 object ex1 {
 
+  def calculate(xs: Array[String]): Double = {
+    val unaryMap = Map(
+      "sin" -> sin _,
+      "cos" -> cos _,
+      "tan" -> tan _,
+      "sqrt" -> sqrt _,
+      "log" -> log _,
+      "exp" -> exp _)
+    val binaryMap = Map(
+      "+" -> ((x:Double,y:Double) => x+y),
+      "-" -> ((x:Double,y:Double) => x-y),
+      "*" -> ((x:Double,y:Double) => x*y),
+      "/" -> ((x:Double,y:Double) => x/y),
+      "^" -> pow _)
+
+   if(xs.length == 2) {
+     val (f, x) = (unaryMap(xs(0)), xs(1).toDouble)
+     f(x)
+   }
+   else if(xs.length == 3) {
+     val (f, x, y) = (binaryMap(xs(1)), xs(0).toDouble, xs(2).toDouble)
+     f(x,y)
+   }
+   else 0.0
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(calculate(Array("2", "+", "3")))
+    println(calculate(Array("sin", "0.23")))
+
+  }
 }
--- ex2.scala
+++ ex2.scala
@@ -1,3 +1,22 @@
+import scala.annotation.tailrec
+
 object ex2 {
 
+  def minmax(xs: List[Int]) = {
+    @tailrec
+    def _minmax(_xs: List[Int], min: Int, max: Int): Tuple2[Int, Int] = {
+      if(_xs.nonEmpty) {
+        _minmax(_xs.tail,
+          if(_xs.head < min) _xs.head else min,
+          if(_xs.head > max) _xs.head else max)
+      } else (min, max)
+    }
+
+    _minmax(xs, xs.head, xs.head)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val (x,y) = minmax(List(4, 6, 1, 9, 6, 1, 8, 6, 4, 2, 9 ,1 ,3 ,5, 10, 0,6, 7))
+    println(x,y)
+  }
 }
